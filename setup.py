fda77df3c06038218ed849f4c76bef7f64757cfb
Wil Tan <wil@dready.org>
2011-05-15 00:01:42 +1000

    added classifiers

--- setup.py
+++ setup.py
@@ -10,6 +10,17 @@ setup(
     author_email = "wil@dready.org",
     description = "Django runserver replacement that reuses its listening socket on reload",
     license = "MIT/X",
-    install_requires = [],
+    # install_requires = ['Django>=1.3'],
     packages = find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Framework :: Django',
+    ]
 )
