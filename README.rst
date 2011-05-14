2f3abed142b2948ee5d8803ffab111bdd10bab63
Wil Tan <wil@dready.org>
2011-05-15 01:16:41 +1000

    Django 1.2.x compatibility

--- README.rst
+++ README.rst
@@ -4,7 +4,7 @@ django-pserver
 This is a drop-in replacement for Django's runserver command for the impatient.
 
 Django's development server restarts itself whenever it detects a change in any loaded module, which is great.
-What's not so great is that during the restart, there is a window of time where, if you're like me, you'd be hitting Ctrl+R key repeatedly on the browser only to be greeted with an error because the server is not ready.
+What's not so great is that during the restart, there is a window of time where, if you're like me, you'd be hitting Ctrl+R key repeatedly on the browser only to be greeted with ``ECONNREFUSED`` because the server is not ready.
 
 Django-pserver solves this problem by reusing the listening socket when it restarts, so you can just hit refresh once and wait (retaining your sanity.)
 
@@ -15,4 +15,6 @@ To use it, simply add ``pserver`` to your ``INSTALLED_APPS``, and run ``manage.p
 
 TODO
 ----
-This only works on Django 1.3.x. Tested on OS X, but should work on any UNIX variant. It will probably break horribly on Windows.
+I've tested this on Django 1.2.4 and 1.3, but it could well work with other versions.
+
+Tested on OS X only, but should work on any UNIX variant. It will probably break horribly on Windows.
--- pserver/management/commands/runserver.py
+++ pserver/management/commands/runserver.py
@@ -2,15 +2,19 @@ from optparse import make_option
 import os
 import sys
 import socket
-import fcntl
 
+from django.core.management.base import CommandError
 from django.core.management.commands.runserver import Command as RunServerCommand
 from django.core.handlers.wsgi import WSGIHandler
 from django.core.servers.basehttp import AdminMediaHandler, WSGIServerException, WSGIServer, WSGIRequestHandler
 from django.utils import autoreload
+from pserver import __version__
 
 
+# store the socket at module level so it can be shared between parent a child process
 PERSISTENT_SOCK = None
+
+
 
 
 class Command(RunServerCommand):
@@ -18,13 +22,150 @@ class Command(RunServerCommand):
     help = "Starts a persistent web server that reuses its listening socket on reload."
 
 
+    def handle(self, addrport='', *args, **options):
+        self.new_sock = False
+        if hasattr(RunServerCommand, 'inner_run'):
+            self.has_ipv6_support = True
+            # Django 1.3-ish, our overwritten self.run and self.inner_run functions should work
+            return super(Command, self).handle(addrport, *args, **options)
+        else:
+            self.has_ipv6_support = False
+            self.use_ipv6 = False # no IPv6 support at that time
+            return self.handle_pre13(addrport, *args, **options)
+
+
+    def init_sock(self):
+        global PERSISTENT_SOCK
+        existing_fd = os.environ.get('SERVER_FD')
+        if not existing_fd:
+            PERSISTENT_SOCK = socket.socket(socket.AF_INET6 if self.use_ipv6 else socket.AF_INET,
+                                            socket.SOCK_STREAM)
+            os.environ['SERVER_FD'] = str(PERSISTENT_SOCK.fileno())
+            self.new_sock = True
+        else:
+            if not self.new_sock:
+                print "Reusing existing socket (fd=%s)" % existing_fd
+            PERSISTENT_SOCK = socket.fromfd(int(existing_fd),
+                                            socket.AF_INET6 if self.use_ipv6 else socket.AF_INET,
+                                            socket.SOCK_STREAM)
+
+    def run_wsgi_server(self, addr, port, handler):
+        """ replaces ``django.core.servers.basehttp.run`` """
+        kwargs = dict(bind_and_activate=False)
+        if self.has_ipv6_support:
+            kwargs['ipv6'] = self.use_ipv6
+        httpd = WSGIServer((addr, port), WSGIRequestHandler, **kwargs)
+        # patch the socket
+        httpd.socket = PERSISTENT_SOCK
+
+        try:
+            httpd.server_bind()
+        except WSGIServerException, e:
+            if 'Errno 22' in str(e):
+                # may have been bound, just emulate some stuff done in server_bind (like setting up environ)
+                httpd.server_name = socket.getfqdn(addr)
+                httpd.server_port = port
+                httpd.setup_environ()
+            else:
+                raise
+        httpd.server_activate()
+        httpd.set_app(handler)
+        httpd.serve_forever()
+
+
+    def handle_pre13(self, addrport, *args, **options):
+        """ modified version of ``runserver.Command.handle`` from Django 1.2.3's (r14552) """
+        import django
+        from django.core.servers.basehttp import run, AdminMediaHandler, WSGIServerException
+        from django.core.handlers.wsgi import WSGIHandler
+        try:
+            from django.contrib.staticfiles.handlers import StaticFilesHandler
+        except ImportError:
+            StaticFilesHandler = None
+
+        if args:
+            raise CommandError('Usage is runserver %s' % self.args)
+        if not addrport:
+            addr = ''
+            port = '8000'
+        else:
+            try:
+                addr, port = addrport.split(':')
+            except ValueError:
+                addr, port = '', addrport
+        if not addr:
+            addr = '127.0.0.1'
+
+        if not port.isdigit():
+            raise CommandError("%r is not a valid port number." % port)
+
+        use_reloader = options.get('use_reloader', True)
+        admin_media_path = options.get('admin_media_path', '')
+        shutdown_message = options.get('shutdown_message', '')
+        use_static_handler = options.get('use_static_handler', True)
+        insecure_serving = options.get('insecure_serving', False)
+        quit_command = (sys.platform == 'win32') and 'CTRL-BREAK' or 'CONTROL-C'
+
+        self.init_sock()
+
+        def inner_run():
+            from django.conf import settings
+            from django.utils import translation
+            print "Validating models..."
+            self.validate(display_num_errors=True)
+            print "\nDjango version %s, using settings %r" % (django.get_version(), settings.SETTINGS_MODULE)
+            print "pserver %s is running at http://%s:%s/" % (__version__, addr, port)
+            print "Quit the server with %s." % quit_command
+
+            # django.core.management.base forces the locale to en-us. We should
+            # set it up correctly for the first request (particularly important
+            # in the "--noreload" case).
+            translation.activate(settings.LANGUAGE_CODE)
+
+            try:
+                handler = WSGIHandler()
+                allow_serving = (settings.DEBUG and use_static_handler or
+                    (use_static_handler and insecure_serving))
+
+                if StaticFilesHandler:
+                    if (allow_serving and
+                            "django.contrib.staticfiles" in settings.INSTALLED_APPS):
+                        handler = StaticFilesHandler(handler)
+                # serve admin media like old-school (deprecation pending)
+                handler = AdminMediaHandler(handler, admin_media_path)
+                self.run_wsgi_server(addr, int(port), handler)
+            except WSGIServerException, e:
+                # Use helpful error messages instead of ugly tracebacks.
+                ERRORS = {
+                    13: "You don't have permission to access that port.",
+                    98: "That port is already in use.",
+                    99: "That IP address can't be assigned-to.",
+                }
+                try:
+                    error_text = ERRORS[e.args[0].args[0]]
+                except (AttributeError, KeyError):
+                    error_text = str(e)
+                sys.stderr.write(self.style.ERROR("Error: %s" % error_text) + '\n')
+                # Need to use an OS exit because sys.exit doesn't work in a thread
+                os._exit(1)
+            except KeyboardInterrupt:
+                if shutdown_message:
+                    print shutdown_message
+                sys.exit(0)
+
+        if use_reloader:
+            from django.utils import autoreload
+            autoreload.main(inner_run)
+        else:
+            inner_run()
+
     def run(self, *args, **options):
         """
         Runs the server, using the autoreloader if needed
         """
         global PERSISTENT_SOCK
+        
         use_reloader = options.get('use_reloader', True)
-        self.new_sock = False
 
         existing_fd = os.environ.get('SERVER_FD')
         if not existing_fd:
@@ -55,10 +196,11 @@ class Command(RunServerCommand):
         self.validate(display_num_errors=True)
         self.stdout.write((
             "Django version %(version)s, using settings %(settings)r\n"
-            "pserver is running at http://%(addr)s:%(port)s/\n"
+            "pserver %(ps_version)s is running at http://%(addr)s:%(port)s/\n"
             "Quit the server with %(quit_command)s.\n"
         ) % {
             "version": self.get_version(),
+            "ps_version": __version__,
             "settings": settings.SETTINGS_MODULE,
             "addr": self._raw_ipv6 and '[%s]' % self.addr or self.addr,
             "port": self.port,
